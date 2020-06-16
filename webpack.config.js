771df8be9c85731f0fa961b5e307a66f7607fb34
Vec7or <carlo@kirchmeier.co>
2020-06-16 15:34:20 +0200

    #3 finished production assets.

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require( 'webpack' );
 const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
@@ -7,7 +8,35 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'resize.js'
   },
+  module: {
+		rules: [
+			{
+				test: /\.(js|jsx|mjs)$/,
+				exclude: /(node_modules|bower_components)/,
+				use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
+          }
+				},
+			},
+		],
+	},
   plugins: [
+    new webpack.optimize.UglifyJsPlugin( {
+			compress: {
+				warnings: false,
+			},
+			mangle: {
+				safari10: true,
+				except: ['__', '_n', '_x', '_nx' ],
+			},
+			output: {
+				comments: false,
+				ascii_only: true,
+			},
+			sourceMap: false,
+		}),
     new CopyPlugin([
         { from: 'src/admin-page/', to: 'admin-page/' },
         { from: 'src/db/', to: 'db/' },
